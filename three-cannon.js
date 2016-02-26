8899035fb9f58bf4cb998a7763f46cb28477af89
Neo <neolwc@gmail.com>
2016-02-26 15:50:29 -0500

    Optimize three-cannon.js and stay DRY

--- three-cannon.js
+++ three-cannon.js
@@ -76,42 +76,14 @@ function Sphere (obj) {
 
 function Box (obj) {
 	this.shape = new CANNON.Box(new CANNON.Vec3(obj.size.x/2, obj.size.y/2, obj.size.z/2));
-	this.body = new CANNON.Body({
-		position:obj.position || {x:0, y:0, z:0},
-		mass:obj.mass || 0,
-		shape:this.shape
-	});
 	this.geometry = new THREE.BoxGeometry(obj.size.x, obj.size.y, obj.size.z);
-	this.mesh = new THREE.Mesh(this.geometry, obj.material || new THREE.MeshNormalMaterial({side:THREE.DoubleSide}));
-	this.mesh.castShadow = true;
-	this.mesh.receiveShadow = true;
-	obj.world.add(this.body);
-	obj.scene.add(this.mesh);
-	this.update = function () {
-		this.mesh.position.copy(this.body.position);
-		this.mesh.quaternion.copy(this.body.quaternion);
-	}
-	objAry.push(this);
+	Obj3d.call(this, obj, this.shape, this.geometry);
 }
 
 function Plane (obj) {
 	this.shape = new CANNON.Plane();
-	this.body = new CANNON.Body({
-		position:obj.position || {x:0, y:0, z:0},
-		mass:obj.mass || 0,
-		shape:this.shape
-	});
 	this.geometry = new THREE.PlaneGeometry(obj.size.x, obj.size.y);
-	this.mesh = new THREE.Mesh(this.geometry, obj.material || new THREE.MeshNormalMaterial({side:THREE.DoubleSide}));
-	this.mesh.castShadow = true;
-	this.mesh.receiveShadow = true;
-	obj.world.add(this.body);
-	obj.scene.add(this.mesh);
-	this.update = function () {
-		this.mesh.position.copy(this.body.position);
-		this.mesh.quaternion.copy(this.body.quaternion);
-	}
-	objAry.push(this);
+	Obj3d.call(this, obj, this.shape, this.geometry);
 }
 
 function Floor (obj) {
